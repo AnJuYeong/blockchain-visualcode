b0b34eb32b5aa680853ee05a0d4945406229d43c
AnJuYeong <dksenfn7907@naver.com>
2022-08-08 19:05:01 +0900

    20220808 쿠키랑세션

--- 20220728test/app.js
+++ 20220728test/app.js
@@ -22,14 +22,22 @@ app.get("/", (req, res) => {
     });
 });
 
+// 클라이언트가 접속 했을 때
 io.on("connection",(socket) => {
     socket.on("joinRoom",(room, name) =>{
+        // 방개념으로 접속 시켜주는 함수 join(방이름)
         socket.join(room);
-        io,to(room).emit("joinRoom", room, name);
+        // to(room) 현재 그 방에 있는 클라이언트에게 요청
+        io.to(room).emit("joinRoom", room, name);
     });
     
     socket.on("leaveRoom", (room, name) => {
+        // 방개념으로 떠나게 해주는 함수 leave(방 이름)
         socket.leave(room);
         io.to(room).emit("leaveRoom", room, name);
+    });
+
+    socket.on("chat",(room, name, msg) => {
+        io.to(room).emit("chat",name,msg)
     });
 });
