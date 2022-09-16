e67f5b28864309a32aa2468fe25fd1aba1151470
AnJuYeong <dksenfn7907@naver.com>
2022-09-16 17:35:34 +0900

    20220916 지뢰찾기 만들기 실패..

--- 20220915/test/src/App.js
+++ 20220915/test/src/App.js
@@ -3,16 +3,38 @@ import Block from './components/Block';
 import Bomb from './components/Bomb';
 import {img1} from "./img"
 function App() {
-  // const arr = [1,2,3,4,5,6,7,8,9,10];
+  
+    const arr = [];
+    for(let i = 0; i < 5; i++){
+      let count = Math.floor(Math.random() * 25);
+      while(!(arr.indexOf(count) === -1)){
+        count = Math.floor(Math.random() * 25)
+      }
+      arr.push(count);
+    }
+  
+    let option = "";
+    //
+    // ㅜ 폭탄의 조건을 담고
+    const isBomb = true;
+    if (isBomb) {
+      option = "bomp";
+    }
+    
+    for(let i = 0; i < 25; i++){
+      <Bomb option={option} index = {i}/>
+    }
+  
   return (
     <div className="App">
       {/* 배열의 갯수만큼 반복하면서 컴포넌트 생성 및 props 전달 */}
       {/* {arr.map((el) => (<Block num={el}/>))} */}
       {/* <img src={img1}></img> */}
       {/* <Block num = {0}/> */}
-      <Bomb num1="25" Cname="bb"/>
+        <bommb/>
     </div>
   );
 }
+
 
 export default App;
