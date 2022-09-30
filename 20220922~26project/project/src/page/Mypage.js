445628500e89b18d90d6f2dfd996b3d38fb76959
AnJuYeong <dksenfn7907@naver.com>
2022-09-30 17:44:22 +0900

    20220930

--- 20220922~26project/project/src/page/Mypage.js
+++ 20220922~26project/project/src/page/Mypage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Header } from '../com'
 
-const Mypage = () => {
+const Mypage = ({username}) => {
+
   return (
-    <div>gdgd</div>
+    <>
+    <Header></Header>
+    <div>{username}님 페이지에요</div>
+    </>
   )
 }
 
