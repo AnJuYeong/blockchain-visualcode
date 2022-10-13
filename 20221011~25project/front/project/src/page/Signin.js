179e206c4505ffebfb29d6047bb7f6ad8f5089ae
AnJuYeong <dksenfn7907@naver.com>
2022-10-13 17:38:53 +0900

    20221013 리액트 프로젝트 3일차

---
+++ 20221011~25project/front/project/src/page/Signin.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Signin = () => {
+    const nav = useNavigate();
+
+    const login = () => {
+        
+        nav("/");
+    }
+    const signup = () => {
+        nav("/signup");
+    }
+
+  return (
+    <>
+    <div className='main-login'>
+        <div className='login-box'>
+            <div className='login-input'>
+                <div className='login-text'>아이디</div>
+                <input className='login-ip'/>
+                <div className='login-text'>비밀번호</div>
+                <input className='login-ip'/>
+            </div>
+            <div className='login-btn-box'>
+                <button onClick={login} className='login-btn'>로그인</button>
+                <button onClick={signup} className='login-btn'>회원가입</button>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Signin
